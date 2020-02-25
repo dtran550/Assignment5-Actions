2c490639f2943a39d00bab2392341aefbc9a7adc
Daniel Tran <dtran550@gmail.com>
2020-02-25 06:03:36 -0800

    Finished with unit test and function of first and last element returning function

--- task.py
+++ task.py
@@ -9,3 +9,10 @@ def areaCircle(radius):
     if (radius < 0):
         return None
     return radius * radius * math.pi
+
+
+def firstLastListElem(intList):
+    # If list is empty, it is treated as a boolean False, and we have it return None
+    if not intList:
+        return None
+    return intList[0], intList[-1]
--- tests.py
+++ tests.py
@@ -23,6 +23,27 @@ class TestCase(unittest.TestCase):
         radius = 0
         self.assertEqual(0, task.areaCircle(radius))
 
+    def test_firstLastListElem(self):
+        # Setting up environmental variables which consist of lists of varying length
+        listEmpty = []
+        listOfOne = [2]
+        listOfTTwo = [1, 2]
+        listOfThree = [1, 2, 3]
+        # Expecting None when function is passed an empty list
+        assertEqual(None, firstLastListElem(listEmpty))
+        # Function returns a tuple of first and last value, so call assert on
+        # first element, and second element of returned object of function
+        assertEqual(2, firstLastListElem(listOfOne[0]))
+        assertEqual(2, firstLastListElem(listOfOne[1]))
+        # Expecting first and last element to be first and second element of
+        # the list passed in to Function
+        assertEqual(1, firstLastListElem(listOfTTwo)[0])
+        assertEqual(2, firstLastListElem(listOfTTwo)[1])
+        # Expecting first and last element of list passed in to function to be
+        # correct
+        assertEqual(1, firstLastListElem(listOfTThree)[0])
+        assertEqual(3, firstLastListElem(listOfTThree)[3])
+
 
 if __name__ == '__main__':
     unittest.main()
