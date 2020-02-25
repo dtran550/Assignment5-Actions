abb47fe5d58886e2f18981fc034ca649b35bbabf
Daniel Tran <dtran550@gmail.com>
2020-02-25 05:22:35 -0800

    Added unit test for the area of a circle calculation, and the area of a circle function itself

--- task.py
+++ task.py
@@ -1,2 +1,10 @@
+import math
+
+
 def firstrun():
     return "success"
+
+def areaCircle(radius):
+    if (radius < 0):
+        return None
+    return radius * radius * math.pi
--- tests.py
+++ tests.py
@@ -11,6 +11,20 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def test_areaCircle(self):
+        # Setting up environmental variables to use for test of areaCircle()
+        radius = 10
+        expected = radius * radius * task.math.pi
+        self.assertEqual(expected, task.areaCircle(radius))
+        # Testing case where radius provided is negative
+        radius = - 10
+        self.assertEqual(None, task.areaCircle(radius))
+        # Testing case where radius provided is negative
+        radius = 0
+        self.assertEqual(0, task.areaCircle(radius))
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
